2eb39e82b745861f8ee38d13b6590af5ce00acdd
Craig Fowler <craig@wearegiants.com>
2016-02-27 13:38:08 -0800

    single

--- single.php
+++ single.php
@@ -14,9 +14,8 @@ $thumb_url = $thumb_url_array[0];
 	<header class="hentry__header">
 		<div class="fs-row">
 			<div class="fs-cell fs-lg-8 fs-md-5 fs-sm-3 fs-centered">
-				<div class="accent accent--sm"><?php the_breadcrumb(); ?></div>
-				<h1 class="hentry__title title--lg"><?php the_title(); ?></h1>
-				<div class="accent accent--sm">Words by <?php the_author(); ?>, posted on <?php the_date(); ?></div>
+				<h1 class="hentry__title title--lg text-center"><?php the_title(); ?></h1>
+				<div class="accent accent--sm text-center">Words by <?php the_author(); ?>, posted on <?php the_date(); ?></div>
 			</div>
 		</div>
 	</header>
@@ -24,9 +23,40 @@ $thumb_url = $thumb_url_array[0];
 	<div class="hentry__content">
 		<div class="fs-row">
 			<div class="fs-cell fs-lg-8 fs-md-5 fs-sm-3 fs-centered">
-				<?php the_content(); ?>
+				<div class="emphasized"><?php the_content(); ?></div>
+				<?php $blog = get_field('blog'); ?>
 			</div>
 		</div>
+		<?php if($blog): ?>
+		<?php foreach($blog as $field): ?>
+		<?php if($field['text']): ?>
+		<div class="fs-row">
+			<div class="fs-cell fs-lg-8 fs-md-5 fs-sm-3 fs-centered">
+				<?php echo $field['text']; ?>
+			</div>
+		</div>
+		<?php endif; ?>
+
+			<?php $gallery = $field['gallery']; ?>
+			<?php if($gallery): ?>
+			<hr class="invisible compact">
+			<div class="carousel-wrapper hentry__carousel-wrapper">
+				<div class="fs-row">
+					<div class="fs-cell fs-lg-11 fs-md-5 fs-sm-3 fs-centered">
+						<div class="hentry__carousel carousel" data-carousel-options='{"contained":false, "autoHeight": true}'>
+<?php foreach($gallery as $image): ?>
+							<div class="hentry__carousel-slide">
+								<img src="<?php echo $image['sizes']['gallery-xl']; ?>" class="img-responsive" />
+							</div>
+<?php endforeach; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php endif; ?>
+
+		<?php endforeach; ?>
+		<?php endif; ?>
 	</div>
 </article>
 
