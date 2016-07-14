37f37f53d944be33c7092a2d51728b21832ec290
Craig Fowler <craig@wearegiants.com>
2016-07-13 18:10:11 -0700

    subscribe stuffs

--- parts/footer-mailchimp.php
+++ parts/footer-mailchimp.php
@@ -1,8 +1,10 @@
 <div id="mailchimp-modal" class="mfp-hide modal">
 <img src="/assets/img/mailing-01.svg" class="subscribe-svg subscribe-svg__top">
-<div class="wrapper wrapper--extra bg--red">
+<div class="subscribe-wrapper bg--red">
 <a class="modal__close popup-modal-dismiss ss-gizmo ss-delete subscribe-close" href="#"></a>
 <h1 class="modal__title color--white text-center">Sign up for our mailing list:</h1>
+<input type="text" class="subscribe-input" placeholder="Email Address">
+<input type="submit" value="Submit" class="subscribe-form-submit bg--white color--red ss-gizmo ss-right right">
 </div>
 <img src="/assets/img/mailing-02.svg" class="subscribe-svg subscribe-svg__bottom">
 </div>
