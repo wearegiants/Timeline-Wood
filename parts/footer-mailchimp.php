bc9e226cd3e5f4bf2b94dd5c26afd1557e26803f
Craig Fowler <craig@wearegiants.com>
2016-07-13 18:56:43 -0700

    subscribe changes

--- parts/footer-mailchimp.php
+++ parts/footer-mailchimp.php
@@ -3,8 +3,8 @@
   <div class="subscribe-wrapper bg--red">
     <a class="modal__close popup-modal-dismiss ss-gizmo ss-delete subscribe-close" href="#"></a>
     <h1 class="modal__title color--white text-center">Sign up for our mailing list:</h1>
-    <form>
-      <input type="text" class="subscribe-input" placeholder="Email Address">
+    <form class="subscribe-form">
+      <input type="text" class="subscribe-input color--black" placeholder="Email Address">
       <input type="submit" value="Submit" class="subscribe-form-submit bg--white color--red ss-gizmo ss-right right">
     </form>
   </div>
