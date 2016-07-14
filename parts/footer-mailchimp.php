c662c0d274df75ccb7a3c69d877aaf5e487e2596
Craig Fowler <craig@wearegiants.com>
2016-07-13 18:49:21 -0700

    stuff

--- parts/footer-mailchimp.php
+++ parts/footer-mailchimp.php
@@ -1,10 +1,12 @@
 <div id="mailchimp-modal" class="mfp-hide modal">
-<img src="/assets/img/mailing-01.svg" class="subscribe-svg subscribe-svg__top">
-<div class="subscribe-wrapper bg--red">
-<a class="modal__close popup-modal-dismiss ss-gizmo ss-delete subscribe-close" href="#"></a>
-<h1 class="modal__title color--white text-center">Sign up for our mailing list:</h1>
-<input type="text" class="subscribe-input" placeholder="Email Address">
-<input type="submit" value="Submit" class="subscribe-form-submit bg--white color--red ss-gizmo ss-right right">
-</div>
-<img src="/assets/img/mailing-02.svg" class="subscribe-svg subscribe-svg__bottom">
+  <img src="/assets/img/mailing-01.svg" class="subscribe-svg subscribe-svg__top">
+  <div class="subscribe-wrapper bg--red">
+    <a class="modal__close popup-modal-dismiss ss-gizmo ss-delete subscribe-close" href="#"></a>
+    <h1 class="modal__title color--white text-center">Sign up for our mailing list:</h1>
+    <form>
+      <input type="text" class="subscribe-input" placeholder="Email Address">
+      <input type="submit" value="Submit" class="subscribe-form-submit bg--white color--red ss-gizmo ss-right right">
+    </form>
+  </div>
+  <img src="/assets/img/mailing-02.svg" class="subscribe-svg subscribe-svg__bottom">
 </div>
