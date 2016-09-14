e0b083225788055298f0399685ece8c36986f654
Craig Fowler <craig@wearegiants.com>
2016-09-14 16:40:22 -0700

    updated stuff

--- parts/header-products.php
+++ parts/header-products.php
@@ -7,7 +7,7 @@
 <?php foreach ($rows as $row):?>
 <?php $image = $row['thumbnail']; ?>
           <div class="fs-cell fs-lg-fourth">
-            <a href="<?php echo $row['url']; ?>" class="product-drop__link">
+            <a href="<?php echo 'http://timeline-wood.myshopify.com/collections/skinnies/' #echo $row['url']; ?>" class="product-drop__link">
               <div class="product-drop__thumb"><img src="<?php echo $image['sizes']['medium']; ?>" class="img-responsive" alt="<?php echo $row['title']; ?>" /></div>
               <h4><?php echo $row['title']; ?></h4>
             </a>
