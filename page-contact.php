75023a6fabebcba636e0ff00d8f996e58ec8fc0a
Craig Fowler <craig@wearegiants.com>
2016-11-18 12:06:39 -0800

    updated

--- page-contact.php
+++ page-contact.php
@@ -21,17 +21,35 @@
 <?php #Contact ?>
 <div class="fs-row">
 	<div class="fs-cell fs-lg-3 fs-md-2 fs-sm-3"><?php the_field('description'); ?></div>
-	<div class="fs-cell fs-lg-8 fs-lg-push-1 fs-md-4 fs-sm-3"></div>
+	<div class="fs-cell fs-lg-8 fs-lg-push-1 fs-md-4 fs-sm-3">
+		<div class="contact-map">
+			<?php the_field('map'); ?>
+		</div>
+	</div>
 </div>
 
-<hr class="invisible">
+<hr class="invisible big">
 <?php #Join the Newsletter ?>
+<?php
+  $form_action = get_field('mailchimp_action_url', 11);
+?>
 <div class="fs-row">
 	<div class="fs-cell fs-lg-3 fs-md-2 fs-sm-3"><?php the_field('join'); ?></div>
-	<div class="fs-cell fs-lg-8 fs-lg-push-1 fs-md-4 fs-sm-3"></div>
+	<div class="fs-cell fs-lg-8 fs-lg-push-1 fs-md-4 fs-sm-3">
+		<form class="contact__subscribe" action="<?php echo $form_action; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+      <div class="input-group">
+        <input type="email" value="" placeholder="Enter your email address" name="EMAIL" id="Email" class="contact__subscribe-text color--black" autocorrect="off" autocapitalize="off">
+        <span class="input-group-btn">
+          <button type="submit" class="subscribe-form-submit bg--orange color--white ss-gizmo ss-right right" name="subscribe" id="Subscribe">
+            <span class="newsletter__submit-text--large">Submit</span>
+          </button>
+        </span>
+      </div>
+    </form>
+	</div>
 </div>
 
-<hr class="invisible">
+<hr class="invisible big">
 <?php #Contact ?>
 <div class="fs-row">
 	<div class="fs-cell fs-lg-3 fs-md-2 fs-sm-3"><?php the_field('commercial_inquiries'); ?></div>
