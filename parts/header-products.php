0301649f841be1a872d9b3efaaaac3c4b403df4f
Craig Fowler <craig@wearegiants.com>
2017-01-05 08:21:14 -0800

    added some stuff

--- parts/header-products.php
+++ parts/header-products.php
@@ -1,7 +1,7 @@
 <div id="header-products" class="product-drop text-center">
   <div class="fs-row relative">
     <div class="fs-cell fs-lg-10 fs-md-full fs-sm-3 fs-centered">
-      <div class="wrapper bg--black-75">
+      <div class="">
         <div class="fs-row">
 <?php $rows = get_field('product_collections',4); ?>
 <?php $items = count($rows); ?>
@@ -24,13 +24,15 @@
     $lgPush = 'fs-lg-fourth';
   }
 ?>
-          <div class="fs-cell <?php echo $lgPush; ?> product-<?php echo $i; ?>">
-            <a href="<?php echo 'http://timeline-wood.myshopify.com/collections/skinnies/' #echo $row['url']; ?>" class="product-drop__link">
-              <div class="product-drop__thumb">
-                <img src="<?php echo $image['sizes']['gallery-sm']; ?>" class="img-responsive" alt="<?php echo $row['title']; ?>" />
-              </div>
-              <h4 class="color--white"><?php echo $row['title']; ?></h4>
-            </a>
+          <div class="fs-cell <?php echo $lgPush; ?> fs-contained product-drop__item product-<?php echo $i; ?>">
+            <div class="wrapper">
+              <a href="<?php echo 'http://timeline-wood.myshopify.com/collections/skinnies/' #echo $row['url']; ?>" class="product-drop__link">
+                <div class="product-drop__thumb">
+                  <img src="<?php echo $image['sizes']['gallery-sm']; ?>" class="img-responsive" alt="<?php echo $row['title']; ?>" />
+                </div>
+                <h4 class="color--white"><?php echo $row['title']; ?></h4>
+              </a>
+            </div>
           </div>
 <?php $i++; endforeach; ?>
         </div>
