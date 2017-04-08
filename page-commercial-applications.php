732cf960138446a0c1bf45325c4df0f2499e264c
Craig Fowler <craig@wearegiants.com>
2017-04-08 11:39:14 -0700

    rearranged comm app page

--- page-commercial-applications.php
+++ page-commercial-applications.php
@@ -11,7 +11,7 @@
     'post_type'      => 'page',
     'posts_per_page' => -1,
     'post_parent'    => $post->ID,
-    'order'          => 'ASC',
+    'order'          => 'DESC',
     'orderby'        => 'menu_order'
   );
   $home_posts = get_posts( $home_args );
