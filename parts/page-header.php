dbf715d1d514f6f10a77ed200627a683262502a4
Craig Fowler <craig@howler.la>
2017-07-19 16:37:51 -0700

    thangs

--- parts/page-header.php
+++ parts/page-header.php
@@ -1,3 +1,13 @@
 <div class="page-header">
   <h2 class="title title--md text-center capped spaced nomargin color--black"><?php the_title(); ?></h2>
+  <?php 
+    $thecontent = get_the_content();
+    if(!empty($thecontent)): 
+  ?>
+  <div class="page-header__content text-center fs-row">
+    <div class="fs-cell fs-lg-8 fs-md-4 fs-sm-3 fs-centered">
+      <?php the_content(); ?>
+    </div>
+  </div>
+  <?php endif; ?>
 </div>
