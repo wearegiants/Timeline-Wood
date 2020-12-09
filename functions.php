4aa3df6bc783f1df4c4fb7a5dbd1a87f1756bc7d
Craig Fowler <craig@howler.la>
2020-12-09 10:18:42 -0800

    removed jquery

--- functions.php
+++ functions.php
@@ -66,3 +66,10 @@ add_theme_support('sage');
 */
 
 new Roots\Acorn\Bootloader();
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', '', false, null);
+   wp_enqueue_script('jquery');
+}
